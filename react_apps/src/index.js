ec00fc1ca303765a230644fd2d6f1620ce2a0a92
shankar <123456789987654321sr@gmail.com>
2021-04-16 00:08:05 +0530

    changed to browser router

--- react_apps/src/index.js
+++ react_apps/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import {BrowserRouter as Router} from 'react-router-dom';
-import {HashRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+//import {HashRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>,
